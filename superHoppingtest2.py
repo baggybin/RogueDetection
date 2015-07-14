b4acd839d5c933d2da2bab99d8f95383f8c9c268
bagsofnarcissism <jonathanoddball@gmail.com>
2015-07-15 06:17:02 +0930

    karma detectionm

--- superHoppingtest2.py
+++ superHoppingtest2.py
@@ -3,6 +3,8 @@ import sys, os, signal
 from multiprocessing import Process
 from scapy.all import *
 import time
+import subprocess
+import re
 '''
 funtiom to randomlu hop through IEEE channels
 may test it with sequential ordering
@@ -13,6 +15,8 @@ class superHoppingtest:
     """Class for a user of the chat client."""
     def __init__(self):
         self.count = 0
+        self.mac =""
+        self.KARMAAP = []
 
 
     def ch_hopp(self):
@@ -32,9 +36,18 @@ class superHoppingtest:
         
     def fakeSSID(self):        
         for i in range(5):
+            
+            
+            
             #gneerate a small 16 character SSID Randomly 
             fakeSSID =''.join(random.choice('0123456789ABCDEF') for i in range(16))
-            print "fakeSSID", fakeSSID
+            
+            print "________________________________"
+            print "Randomly Generated SSID ", fakeSSID
+            print "setting to local interface"
+            print "________________________________"
+            
+            
             ''''
             Brinf the wireless interface down
             set its ESSID to randomly generated one
@@ -52,20 +65,43 @@ class superHoppingtest:
             Next Step try capture an assosiation from IWCONFIG with Fake SSid
             Thsi will Eliminate any to capture thge reponces
             '''
-            
-            #ROUGE = os.system("iwconfig wlan4 | grep 'ESSID:' | awk '{print $4}' | sed 's/ESSID://g' | sed 's/\"//g'")
+                    
             import subprocess
-            #output = subprocess.check_output("iwconfig wlan4 | grep 'ESSID:' | awk '{print $4}' | sed 's/ESSID://g' | sed 's/\"//g'",shell=True )
-            #print output
-            #if i == 20:
-            #    ch_hopp()
-        
-    
-  
-    
+            #call a bash command to iwconfig to search for the generated SSID on that interfacE"
+            output = subprocess.check_output("iwconfig wlan4 | grep 'ESSID:' | awk '{print $4}' | sed 's/ESSID://g' | sed 's/\"//g'",shell=True )
+            from subprocess import Popen, PIPE
+            #call iwconfig on interface and extract the assosiated MAC (BSSID), (which will onl7 exist is there is an assosoiation)
+            
+            output2 = subprocess.check_output("iwconfig wlan4 | grep 'Access Point:' | awk '{print $6}'", shell=True)
+            
+            
+            if (output.strip()) == str(fakeSSID.strip()):
+                if re.match("[0-9a-f]{2}([-:])[0-9a-f]{2}(\\1[0-9a-f]{2}){4}$", output2.lower()):
+                    self.count +=1
+                    print "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+                    print "KARMA ACCESSS POINT DETECTED"
+                    print "Fake SSID ", fakeSSID
+                    print "Assososiatesd  SSID ", output
+                    print "Assosoated Rouge BSSid", output2
+                    if output2 in self.KARMAAP:
+                        print  "KARMA BSSSID Seen Before"
+                    self.KARMAAP.append(output2)
+                    print "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+            
+            
+            print "KARMA Dectected " + str(self.count) + " times"
+            
+            
+
 if __name__ == '__main__':
+    os.system("service network-manager stop")
     S = superHoppingtest()
     S.fakeSSID()
+    
+    print "Restarting Services"
+    os.system("service network-manager start")
+    os.system("dhclient")
+    
     
     #import subprocess
     #p = subprocess.Popen(["python sniffer.py"], stdout=subprocess.PIPE)
