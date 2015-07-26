7c38a7755b053139fece9abad2400026ffa70fee
bagsofnarcissism <jonathanoddball@gmail.com>
2015-07-27 07:43:12 +0930

    sequence and chnage detection without channel hopping

--- sequence/tiny.py
+++ sequence/tiny.py
@@ -25,31 +25,36 @@ OUI
 '''
 class scanning:
     """Class for a user of the chat client."""
-    def __init__(self, intf, count, channel,BSSID, SSID, WIFIDATA,datab):
+    def __init__(self, intf, count, channel,BSSID, SSID, accesspoint):
         self.intf = intf
         self.BSSID = BSSID
         self.SSID = SSID
-        self.WIFIDATA = WIFIDATA
-        self.datab = datab
-        
+        self.accesspoint = accesspoint
         self.seq1 = 0
         self.flag1 = 0
-        
         self.counter = 0
-        self.channel = channel
+        self.channel = accesspoint["channel"]
         self.count = count
         self.accessPointsSQ = []
         self.seq_list = []
-         
+    
+    
+    def channel_change(self):
         os.system("sudo ifconfig %s down" %  self.intf )
         os.system("sudo iw dev "+  self.intf + " set type monitor")
         os.system("sudo ifconfig %s up" %  self.intf )
         try:
             os.system("sudo iw dev %s set channel %d" % (self.intf, self.channel))
-            print "channel Change", channel
+            print "channel Change", self.channel
+            print ""
         except Exception, err :
                print err 
     
+    
+    
+    def set_ch(self, channel):
+        self.channel = channel
+
     
     def ch_hop(self):
            channel = int(random.randrange(1,11))
@@ -86,34 +91,34 @@ class scanning:
         print "------------------Started-----------------------------------------------"
         def PacketHandler(frame):      
           if  frame.haslayer(Dot11) and frame.type == 0 and frame.subtype == 8:
-            
-            #
-            #try:
-            #    val = self.datab.search((where('ssid') == str(frame.info)))
-            #    #print "$$$$$$$$$$$$$$$$$$$    VAL    $$$$$$$$$$$$$$$$$$$$$$$$$$$$$"
-            #    #print val, frame.info
-            #    ch = int(ord(frame[Dot11Elt:3].info))
-            #    if not ch == val["channel"]:
-            #        print "Channel Has changed"
-            #    
-            #except:
-            #    pass
-            
+             
+            ### not much use as scanning the one channel
+            try:
+                val = self.datab.search((where('ssid') == str(frame.info)))
+                #print "$$$$$$$$$$$$$$$$$$$    VAL    $$$$$$$$$$$$$$$$$$$$$$$$$$$$$"
+                #print val, frame.info
+                ch = int(ord(frame[Dot11Elt:3].info))
+                if not ch == val["channel"]:
+                    print "Channel Has changed"
+                
+            except:
+                pass
+            enc = None
             if self.flag1 == 0:
                 result = self.oui(frame.addr2)
                 print "********************    OUI ", result
                 self.flag1 = 1
 
-        
+                
                 ## direcly from Airoscapy
                 capability = frame.sprintf("{Dot11Beacon:%Dot11Beacon.cap%}\
                 {Dot11ProbeResp:%Dot11ProbeResp.cap%}")
+                
                 if re.search("privacy", capability): enc = True
                 else: enc  = False
                 
-                               
-                if not self.WIFIDATA["encrypted"] == enc:
-                    print "Encyption Has been chnaged"
+            if not self.accesspoint["encrypted"] == enc and enc != None:
+                print "the encrpytion has changed"
             
               
             try:
@@ -158,6 +163,8 @@ if __name__ == '__main__':
     os.system("sudo iwconfig "+  interface + " mode managed")
     os.system("sudo ifconfig %s up" %  interface )
     cell = Cell.all(interface)
+    db = TinyDB('db.json')
+    
     
     Auth_AP = {}
     S = []
@@ -173,10 +180,12 @@ if __name__ == '__main__':
             #append this dictionary to a list
         S.append(SSIDS)
     
+    
+    
+    
     ## get choice from the user
     input_var = int(input("Choose: "))
     print "-----------------------------------------"
-
     ap = S[input_var - 1]
     print ap["ssid"]
     print ap["address"]
@@ -184,36 +193,48 @@ if __name__ == '__main__':
     print ap["channel"]
     print "---------------------------------------------"
     
-    print "1: Store Valid AP"
-    input_var = int(input("Choose: "))
+
+    
+    loop = True
+    while loop:
+        try:
+            input_var = int(input("1: Store Valid AP \n2: Disregard and Continue\n:"))     
+            if input_var > 0 and input_var <= 2:
+                loop = False
+        except ValueError:
+            pass
     
     if input_var == 1:
-        db = TinyDB('db.json')
-        db.purge()
-        db.insert(S[input_var - 1])
-        
-        #val = db.search((where('ssid') == ap["ssid"]) & (where('address') == str(ap["address"])))
-        #val = db.search((where('ssid') == 'Zoom') & (where('address') == "aa:bb:cc:dd:ee:ff"))
-        
+        #db.purge()
+        #db.insert(S[input_var - 1])
         if db.search((where('ssid') == ap["ssid"]) & (where('address') == str(ap["address"]))) == []:
-            db.insert(S[input_var - 1])
+            db.insert(ap)
         else:
-            print "already Stored in the database"
+            print "This is already Stored in the database"
+
         
-        
-        #print "val", val
-        
-        print """"
+        '''
         print all database
-        """
+        '''
         print db.all()
-            
-    s = scanning(intf="wlan4", count = 6000, channel =ap["channel"], BSSID=ap["address"],SSID=ap["ssid"],WIFIDATA=ap, datab=db)
-    f = s.sniffAP()
-
-
-
-
-
-
+        
     
+    
+    for ap in db.all():
+        try:
+            print ""
+            print "$$$$$$$$$$$$$$$$$$$$$$$   Sannning -----> " , ap["ssid"]
+            s = scanning(intf="wlan4", count = 300, channel=ap["channel"], BSSID=ap["address"],SSID=ap["ssid"], accesspoint=ap)
+            s.set_ch(ap["channel"])
+            s.channel_change()
+            s.sniffAP()
+        except Exception, err:
+            print(traceback.format_exc())
+     
+    #f = s.sniffAP()
+   # s = scanning(intf="wlan4", count = 6000, channel =ap["channel"], BSSID=ap["address"],SSID=ap["ssid"],WIFIDATA=ap, datab=db)
+
+    #pint 'A' * 2000
+
+
+
