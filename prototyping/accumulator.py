0e1700e73739561d7ec444e8fed79672d57dfb13
bagsofnarcissism <jonathanoddball@gmail.com>
2015-08-19 02:33:33 +0930

    small workarounds for channel hopping in IDS, accumulator (but breaks anything other than KARMA)

--- prototyping/accumulator.py
+++ prototyping/accumulator.py
@@ -1,18 +1,26 @@
 #!/usr/bin/env python
 from collections import Counter
+import traceback
+import sys
+from multiprocessing import Process, Value, Array, Manager
 
 
 class AccessPoint:
-    def __init__(self):    
+    def __init__(self, ID):
+        self.ID = ID
         self.BSSID = None
         self.SSID = None
-        self.sec = None
-        self.ch = None
-        self.KARMA = None
+        self.sec = 0
+        self.ch = 0
+        self.KARMA = 0
         self.KARMA_BSSID = None
-        self.AIRBASE = None
-        self.sqChange = None
-        self.timestampChange = None
+        self.AIRBASE = 0
+        self.sqChange = 0
+        self.timestampChange = 0
+        
+    
+    def getID(self):
+        return self.ID
     
     def setBSSID(self,B ):
         self.BSSID = B
@@ -49,7 +57,11 @@ class AccessPoint:
         self.timestampChange +=1
 
     def getKARMA(self):
-        return self.KARMA_BSSID, self.KARMA
+        return self.KARMA
+    
+    def getKARMABSSID(self ):
+        return self.KARMA_BSSID
+    
     
     def getAirbaseNG(self):
         return self.sqChange
@@ -58,22 +70,65 @@ class AccessPoint:
         return self.timestampChange        
 
 
+import smtplib
+import threading
+from threading import Thread
 
-
-class accumulator:
-    def __init__(self, SSID, BSSID):
-        self.SSID = ""
-        self.BSSID = ""
-        self.cnt = Counter()
-        self.AP = None
-
+class accumulator(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.daemon = True
+        self.Shared_Mem_Dictionary = None
+        self.manager = None
+        
+    def run(self):
+        print "Accumulator Starting\n"
+        while True:
+            try:
+                #print len(self.Shared_Mem_Dictionary)
+                #print bool(self.Shared_Mem_Dictionary)
+                #
+                #d=manager.list(self.Shared_Mem_Dictionary)
+                if bool(self.Shared_Mem_Dictionary) == True:
+                    d=self.manager.list(self.Shared_Mem_Dictionary)
+                    for i in d:
+                        if self.Shared_Mem_Dictionary[i].getKARMA() > 0:
+                            #IiGOmf81PYyenrwDrB86B3A
+                            fromaddr = 'rougedetection@gmail.com'
+                            toaddrs  = 'rougedetection@gmail.com'
+                            SUBJECT  = "KARMA ACCESS POINT DETECTED!!"
+                            text = 'KARMA ACCESS POINT Deteceted'  + str(self.Shared_Mem_Dictionary[i].getKARMABSSID())
+                            msg = 'Subject: %s\n\n%s' % (SUBJECT, text)
+                            username = "rougedetection"
+                            password = "IiGOmf81PYyenrwDrB86B3A"
+                            server = smtplib.SMTP('smtp.gmail.com:587')
+                            server.starttls()
+                            server.login(username,password)
+                            server.sendmail(fromaddr, toaddrs, msg)
+                            server.quit()
+                            print "\nKARMA Alert Emailed!"
+                            del(self.Shared_Mem_Dictionary[i])
+                            
+            except Exception, e:
+                print "----", e
+                print(traceback.format_exc())
+                print(sys.exc_info()[0])
+            
+    def setSMem(self,Shared_Mem_Dictionary, mgr):
+        self.Shared_Mem_Dictionary = Shared_Mem_Dictionary
+        self.manager = mgr
 
 #    
 #if __name__ == '__main__':    
-#    a = accumulator("name","aa:bb:cc:dd:ee:ff",1)
-#    a.create_AP()
-#    a.get_AP().set_ch(1)
+#    a = accumulator()
+#    AP = AccessPoint("KARMA")
+#    AP.setKARMA(3,"aa:bb:cc:dd:ee:ff")
+#    global manager
 #    
-#    print a.get_AP().get_ch()
+#    Shared_Mem_Dictionary = manager.dict()
+#    Shared_Mem_Dictionary[AP.getID()]=AP
+#    a.setSMem(Shared_Mem_Dictionary)
+#    a.start()
+##    
 #    
 
